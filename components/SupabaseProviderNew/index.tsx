69b65d17f2164aa488db4d4e3ce1e3eeb6e10f94
CallumBoase <callum.boase@gmail.com>
2024-10-30 17:00:17 +1100

    Untested basic SupabaseProvider

--- components/SupabaseProviderNew/index.tsx
+++ components/SupabaseProviderNew/index.tsx
@@ -11,15 +11,44 @@ import { DataProvider } from "@plasmicapp/host";
 import { v4 as uuid } from "uuid";
 import { useDeepCompareMemo } from "use-deep-compare";
 
+//Import custom createClient that creates the Supabase client based on component render within Plasmic vs Browser
+import createClient from "../../utils/supabase/component";
+
 import buildSupabaseQueryWithDynamicFilters, {
   type Filter,
   type OrderBy,
 } from "../../utils/buildSupabaseQueryWithDynamicFilters";
 
+type Row = {
+  [key: string]: any;
+};
+
+type Rows = {
+  count?: number;
+  data: Row[] | null;
+};
+
 export interface SupabaseProviderNewProps {
   children: React.ReactNode;
   className?: string;
+  queryName: string;
+  tableName: string;
+  columns: string;
+  filters: Filter[];
+  orderBy: OrderBy[];
+  limit?: number;
+  offset?: number;
+  returnCount?: "none" | "exact" | "planned" | "estimated";
+  onError: (supabaseProviderError: SupabaseProviderError) => void;
 }
+
+type SupabaseProviderError = {
+  errorId: string;
+  summary: string;
+  errorObject: any;
+  actionAttempted: string;
+  rowForSupabase: Row | null;
+};
 
 interface Actions {
   addRow(
@@ -36,8 +65,88 @@ export const SupabaseProviderNew = forwardRef<Actions, SupabaseProviderNewProps>
     const {
       children,
       className,
+      queryName,
+      tableName,
+      columns,
+      filters,
+      orderBy,
+      limit,
+      offset,
+      returnCount,
+      onError,
     } = props;
 
+    const [fetchError, setFetchError] = useState<SupabaseProviderError | null>(null);
+    const memoizedFilters = useDeepCompareMemo(() => filters, [filters]);
+    const memoizedOrderBy = useDeepCompareMemo(() => orderBy, [orderBy]);
+
+    // Function to fetch rows from Supabase
+    const fetchData = async () => {
+
+      setFetchError(null);
+
+      try {
+
+        const supabase = createClient();
+
+        //Build the query with dynamic filters that were passed as props to the component
+        const supabaseQuery = buildSupabaseQueryWithDynamicFilters({
+          supabase,
+          tableName,
+          operation: "select",
+          columns,
+          dataForSupabase: null,
+          filters: memoizedFilters,
+          orderBy,
+          limit,
+          offset,
+          returnCount,
+        });
+
+        const { data, error, count } = await supabaseQuery;
+
+        if (error) {
+          throw error;
+        }
+
+        return { data, error, count };
+
+      } catch (err) {
+        console.error(err);
+        const supabaseProviderError = {
+          errorId: uuid(),
+          summary: "Error fetching records",
+          errorObject: err,
+          actionAttempted: "read",
+          rowForSupabase: null,
+        };
+        setFetchError(supabaseProviderError);
+        if (onError && typeof onError === "function") {
+          onError(supabaseProviderError);
+        }
+        throw err;
+      }
+
+    }
+
+    const {
+      data,
+      //error - will not use see notes in KnackProvider
+      // mutate,
+      isLoading,
+    } = useMutablePlasmicQueryData(
+      [
+        queryName, 
+        JSON.stringify(memoizedFilters), 
+        JSON.stringify(memoizedOrderBy)
+      ], 
+      fetchData,
+      {
+        shouldRetryOnError: false
+      }
+    );
+
+    // Element actions
     useImperativeHandle(ref, () => ({
       addRow: async () => console.log("test"),
     }));
@@ -45,9 +154,11 @@ export const SupabaseProviderNew = forwardRef<Actions, SupabaseProviderNewProps>
     return (
       <div className={className}>
         <DataProvider
-          name="SupabaseProviderNew"
+          name={queryName}
           data={{
-            a: "b",
+            data,
+            isLoading,
+            fetchError,
           }}
         >
           {children}
