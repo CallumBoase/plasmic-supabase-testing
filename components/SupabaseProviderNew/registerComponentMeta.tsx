a5af963bafbdd2a03de96b1fe7da08e5fdeb61ed
CallumBoase <callum.boase@gmail.com>
2024-10-31 13:06:04 +1100

    Initial props for SupabaseProviderNew fetching

--- components/SupabaseProviderNew/registerComponentMeta.tsx
+++ components/SupabaseProviderNew/registerComponentMeta.tsx
@@ -1,6 +1,16 @@
 import { CodeComponentMeta } from "@plasmicapp/host";
 import { SupabaseProviderNewProps } from ".";
 
+// className,
+//       queryName,
+//       tableName,
+//       columns,
+//       filters,
+//       orderBy,
+//       limit,
+//       offset,
+//       returnCount,
+
 export const SupabaseProviderNewMeta : CodeComponentMeta<SupabaseProviderNewProps> = {
   name: "SupabaseProviderNew",
   importPath: "./index",
@@ -20,6 +30,167 @@ export const SupabaseProviderNewMeta : CodeComponentMeta<SupabaseProviderNewProp
             5. Delete this placeholder text (from "children" slot). Then add components to "children" and use the dynamic data as you wish! :)`,
         },
       ],
+    },
+    queryName: {
+      type: "string",
+      required: true,
+    },
+    tableName: {
+      type: "string",
+      required: true,
+    },
+    columns: {
+      type: "string",
+      defaultValue: "*",
+    },
+    filters: {
+      type: "array",
+      itemType: {
+        type: "object",
+        fields: {
+          fieldName: "string",
+          operator: {
+            type: "choice",
+            options: [
+              {
+                value: "eq",
+                label: "is equal to (eq)"
+              },
+              {
+                value: "neq",
+                label: "not equal to (neq)"
+              },
+              {
+                value: "gt",
+                label: "greater than (gt)"
+              },
+              {
+                value: "gte",
+                label: "greater than or equal to (gte)"
+              },
+              {
+                value: "lt",
+                label: "less than (lt)"
+              },
+              {
+                value: "lte",
+                label: "less than or equal to (lte)"
+              },
+              {
+                value: "like",
+                label: "matches a case-sensitive pattern (like)"
+              },
+              {
+                value: "ilike",
+                label: "matches a case-insensitive pattern (ilike)"
+              },
+              {
+                value: "is",
+                label: "is (is)"
+              },
+              {
+                value: "in",
+                label: "is in an array (in)"
+              },
+              {
+                value: "contains",
+                label: "contains every element in (contains)"
+              },
+              {
+                value: "containedBy",
+                label: "contained by (containedby)"
+              },
+              {
+                value: "rangeGt",
+                label: "greater than range (rangeGt)"
+              },
+              {
+                value: "rangeGte",
+                label: "greater than or equal to range (rangeGte)"
+              },
+              {
+                value: "rangeLt",
+                label: "less than range (rangeLt)"
+              },
+              {
+                value: "rangeLte",
+                label: "less than or equal to range (rangeLte)"
+              },
+              {
+                value: "rangeAdjacent",
+                label: "is mutually exclusive to range (rangeAdjacent)"
+              },
+              {
+                value: "overlaps",
+                label: "has an element in common with (overlaps)"
+              },
+              {
+                value: "match",
+                label: "where each { column:value, ... } matches (match)"
+              },
+              {
+                value: "or",
+                label: "that matches at least one PostgREST filter (or)"
+              },
+              {
+                value: "textSearch",
+                label: "matches the query string (textSearch)"
+              },
+              {
+                value: "not",
+                label: "that doesn't match the PostgREST filter (not)"
+              },
+            ]
+          },
+          value: "string",
+          value2: "string",
+        },
+      },
+      description:
+        "Filters to execute during the query. Acceptable values are eq, neq, gt, lt, gte, lte.",
+    },
+    orderBy: {
+      type: "array",
+      itemType: {
+        type: "object",
+        fields: {
+          fieldName: "string",
+          direction: {
+            type: "choice",
+            options:  [
+              {label: "Ascending",value: "asc"}, 
+              {label: "Descending",value: "desc"}
+            ],
+          },
+        },
+      },
+      displayName: "Order by",
+      description:
+        "Columns to order the results by during the query.",
+    },
+    limit: {
+      type: "number",
+      step: 1,
+      min: 0,
+      required: false,
+      description: 'Number of records to fetch',
+      advanced: true,
+    },
+    offset: {
+      type: "number",
+      step: 1,
+      min: 0,
+      required: false,
+      description: 'Number of records to skip',
+      advanced: true,
+    },
+    returnCount: {
+      type: "choice",
+      options: ["none", "exact", "planned", "estimated"],
+      defaultValue: "none",
+      required: false,
+      description: 'Count algorithm to use to count rows in the table or view. `"none"`: Don\'t return a count. `"exact"`: Exact but slow count algorithm. Performs a `COUNT(*)` under the hood. `"planned"`: Approximated but fast count algorithm. Uses the Postgres statistics under the hood. `"estimated"`: Uses exact count for low numbers and planned count for high numbers.',
+      advanced: true,
     },
   }
 };
