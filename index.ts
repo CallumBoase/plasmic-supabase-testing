9a32c1916b619a1225c7a543b6867c4723382782
CallumBoase <callum.boase@gmail.com>
2024-10-31 13:05:17 +1100

    Turn of unecessary components

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-export { SupabaseUserGlobalContext } from "./components/SupabaseUserGlobalContext";
-export { SupabaseUserGlobalContextMeta } from "./components/SupabaseUserGlobalContext/registerComponentMeta";
+// export { SupabaseUserGlobalContext } from "./components/SupabaseUserGlobalContext";
+// export { SupabaseUserGlobalContextMeta } from "./components/SupabaseUserGlobalContext/registerComponentMeta";
 
 export { SupabaseProvider } from "./components/SupabaseProvider";
 export { SupabaseProviderMeta } from "./components/SupabaseProvider/registerComponentMeta";
@@ -7,9 +7,9 @@ export { SupabaseProviderMeta } from "./components/SupabaseProvider/registerComp
 export { SupabaseProviderNew } from "./components/SupabaseProviderNew";
 export { SupabaseProviderNewMeta } from "./components/SupabaseProviderNew/registerComponentMeta";
 
-export { SupabaseUppyUploader } from "./components/SupabaseUppyUploader"
-export { SupabaseUppyUploaderMeta } from "./components/SupabaseUppyUploader/registerComponentMeta"
-export { SupabaseUppyHOC } from "./components/SupabaseUppyUploader"
-export { SupabaseUppyHOCMeta } from "./components/SupabaseUppyUploader/registerComponentMeta"
+// export { SupabaseUppyUploader } from "./components/SupabaseUppyUploader"
+// export { SupabaseUppyUploaderMeta } from "./components/SupabaseUppyUploader/registerComponentMeta"
+// export { SupabaseUppyHOC } from "./components/SupabaseUppyUploader"
+// export { SupabaseUppyHOCMeta } from "./components/SupabaseUppyUploader/registerComponentMeta"
 
 export type { AuthorizationCheckFunction, RoutePath } from "./utils/authorizationCheckFunctionTypes";
